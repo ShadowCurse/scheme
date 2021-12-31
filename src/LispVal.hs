7b89754335bac9dfce45c13f77e5b9397d188492
ShadowCurse <shadowcurse57@gmail.com>
2021-12-31 18:36:41 +0300

    added exceptions

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -6,7 +6,7 @@ module LispVal
   , Eval(..)
   , IFunc(..)
   , EnvCtx(..)
-  -- LispException(..),
+  , LispException(..)
   , showVal
   ) where
 
@@ -58,3 +58,35 @@ newtype IFunc =
 
 instance Eq IFunc where
   (==) _ _ = False
+
+data LispException
+  = NumArgs Integer [LispVal]
+  | LengthOfList T.Text Int
+  | ExpectedList T.Text
+  | TypeMismatch T.Text LispVal
+  | BadSpecialForm T.Text
+  | NotFunction LispVal
+  | UnboundVar T.Text
+  | Default LispVal
+  | PError String -- from show anyway
+  | IOError T.Text
+
+instance Show LispException where
+  show = T.unpack . showError
+
+unwordsList :: [LispVal] -> T.Text
+unwordsList list = T.unwords $  showVal <$> list
+
+showError :: LispException -> T.Text
+showError err =
+  case err of
+    (IOError txt)          -> T.concat ["Error reading file: ", txt]
+    (NumArgs int args)     -> T.concat ["Error Number Arguments, expected ", T.pack $ show int, " recieved args: ", unwordsList args]
+    (LengthOfList txt int) -> T.concat ["Error Length of List in ", txt, " length: ", T.pack $ show int]
+    (ExpectedList txt)     -> T.concat ["Error Expected List in funciton ", txt]
+    (TypeMismatch txt val) -> T.concat ["Error Type Mismatch: ", txt, showVal val]
+    (BadSpecialForm txt)   -> T.concat ["Error Bad Special Form: ", txt]
+    (NotFunction val)      -> T.concat ["Error Not a Function: ", showVal val]
+    (UnboundVar txt)       -> T.concat ["Error Unbound Variable: ", txt]
+    (PError str)           -> T.concat ["Parser Error, expression cannot evaluate: ",T.pack str]
+    (Default val)          -> T.concat ["Error, Danger Will Robinson! Evaluation could not proceed!  ", showVal val]
